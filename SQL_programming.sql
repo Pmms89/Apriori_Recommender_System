65f0f34ced57352f305eed97d027e93c72610cc2
Patrícia Silva <66090235+Pmms89@users.noreply.github.com>
2023-05-12 20:49:19 -0300

    Update SQL_programming.sql

--- SQL_programming.sql
+++ SQL_programming.sql
@@ -1,26 +1,7 @@
-SELECT * FROM Books.books_cleaned;
-
-SELECT COUNT(*) FROM Books.books_cleaned;
-
-SELECT * FROM Books.ratings;
-
-SELECT COUNT(*) FROM Books.ratings;
-
-SELECT COUNT(*) FROM Books.users_cleaned;
- 
- 
  -- Associating the columns
  
- USE BOOKS;
- 
- SELECT * 
- FROM Books.books_cleaned AS bc
- JOIN Books.ratings AS br
- ON bc.ISBN = br.ISBN
- JOIN Books.users_cleaned AS bu
- ON bu.UserID = br.UserID;
- 
- 
+USE BOOKS;
+  
 SELECT bu.UserID, bu.Age, bu.City, bu.Country,
 br.ISBN, br.Book_Rating,
 bc.Book_Title, bc.Book_Author, bc.Year_Of_Publication, bc.Publisher
